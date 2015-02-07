232421bbb59b7e960c040e49d5e180f219796eb3
Guillaume Berard <berardgui@gmail.com>
2015-02-07 09:26:30 +0000

    Echo if quotas

--- webcheckers/check_quota.sh
+++ webcheckers/check_quota.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-curl -s http://www.cic.gc.ca/english/work/iec/data.xml | grep "$1.*$2" -A3 | tail -n 2 | grep -o "[0-9]*"
+quotas=$(curl -s http://www.cic.gc.ca/english/work/iec/data.xml | grep "$1.*$2" -A3 | tail -n 2 | grep -o "[0-9]*")
+
+for q in $quotas
+do
+	if [ "$q" != 0 ]
+	then
+		echo "Quotas are now open"
+		echo "$quotas"
+		break;
+	fi
+done
+
